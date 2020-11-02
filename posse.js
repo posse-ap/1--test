8ab381ffa74be89082e9031e282801a93579305c
Shuto0410 <shuto410@icloud.com>
2020-11-02 16:50:11 +0900

    3rd

--- posse.js
+++ posse.js
@@ -1,3 +1,6 @@
+// ----------ペースト-----------
+
+
 // モーダル
 $(function() {
 
@@ -22,223 +25,229 @@ $(function() {
 
 });
 
-//--------------------- こっから棒グラフ---------------------------------------
-function create_chart_1() {
-    google.load("visualization", "1", { packages: ["corechart"] });
-    google.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-        var dataArray = [
-            ['day', 'time']
-        ];
-        var df = $.Deferred();
+// const button1 = document.getElementById('send');
+// document.getElementsByClassName("modalContents").style.display ="block";
 
-        $(function() {
-            $.ajax({
-                url: 'study_time.json',
-                dataType: 'json',
-            }).done(function(data) {
-                console.log("success");
-                // $(data.study_time).each(function () {
-                // 親要素名？をいれても動く
-                $(data).each(function() {
-                    var data_item = [this.day, this.time];
-                    dataArray.push(data_item);
-                });
-                df.resolve();
-            }).fail(function() {
-                console.log("error");
-            });
-        });
+// button1.addEventListener('click', (e) => {
+//     // TODO:モーダル の中身を非表示にして、数秒間ロード画面を開いた後で、終了画面に移行
+//     // displayをnoneにすればいい？？
+//     const p1 = document.getElementsByClassName("modalContents");
+//     p1.style.display ="none";
+// });
 
-        df.done(function() {
-            var chartdata = google.visualization.arrayToDataTable(dataArray);
+//----------------------------------ロード画面----------------------------------------
 
-            var options = {
-                // title: 'Age vs. Weight comparison',
-                //hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
-                //vAxis: { title: 'Weight', minValue: 0, maxValue: 15 },
-                legend: 'none',
-                'chartArea': { top: 10, 'width': '80%', 'height': '80%' },
-                //hAxis:{showTextEvery:2}
-                //vAxis:{gridlined:{count:7}}
-                // legend:{position:'bottom'}
-                // baselineColor: 'none',
-                // gridlines: 'none',
+$(".btn").on("click", function(){
+    document.getElementById("modal_contents").style.display ="none";
+    $(document).ajaxSend(function() {
+        $("#overlay").fadeIn(500);
+    });
+    $.ajax({
+        type: 'GET',
+        success: function(data){
+            console.log(data);
+        }
+    }).done(function() {
+        setTimeout(function(){
+            $("#overlay").fadeOut(500);
+            document.getElementById("success").style.display ="block";
+        },3000);
+    });
+   
+    return false;
+});
 
-                // baselineColor: "transparent",
-                // gridlines: {
-                //     color: "transparent"
-                // },
-                // textPosition: "none",
-                // vAxes: {
-                //     gridLines: {
-                //         color: '#fffffff'
-                //     }
-                // },
-                vAxis: {
-                    gridlines: {
-                        color: "#ffffff"
-                    },
-                    format: "0h"
-                },
-                hAxis: {
-                    gridlines: {
-                        color: "#ffffff"
-                    }
-                },
-                baselineColor: "transparent",
-
-                colors: ['#1754EF', '#0F71BD', '#20BDDE']
-
-            };
-            var chart = new google.visualization.ColumnChart(document.getElementById('bargraph'));
-
-            chart.draw(chartdata, options);
-        });
+//ーーーーーーーーーーーーーーーーツイッターーーーーーーーーーーーーーー
+document.getElementById("send").addEventListener('click', function(event) {
+    event.preventDefault();
+    var left = Math.round(window.screen.width / 2 - 275);
+    var top = (window.screen.height > 420) ? Math.round(window.screen.height / 2 - 210) : 0;
+    if(document.forms.modal_form.modal_check12.checked){
+        window.open(
+            "https://twitter.com/intent/tweet?text=" + encodeURIComponent(document.getElementById("txtbox").value),
+            null,
+            "scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,left=" + left + ",top=" + top);
     }
+    
+});
+
+
+
+//--------------------- こっから棒グラフ---------------------------------------
+
+google.load("visualization", "1", { packages: ["corechart"] });
+google.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var dataArray = [
+        ['day', 'time']
+    ];
+    var df = $.Deferred();
+
+    $(function() {
+        $.ajax({
+            url: 'study_time.json',
+            dataType: 'json',
+        }).done(function(data) {
+            console.log("success");
+
+            $(data).each(function() {
+                var data_item = [this.day, this.time];
+                dataArray.push(data_item);
+            });
+            df.resolve();
+        }).fail(function() {
+            console.log("error");
+        });
+    });
+
+    df.done(function() {
+        var chartdata = google.visualization.arrayToDataTable(dataArray);
+
+        var options = {
+            legend: 'none',
+            'chartArea': { top: 10, 'width': '80%', 'height': '80%' },
+            vAxis: {
+                gridlines: {
+                    color: "#ffffff"
+                },
+                format: "0h"
+            },
+            hAxis: {
+                gridlines: {
+                    color: "#ffffff"
+                }
+            },
+            baselineColor: "transparent",
+
+            colors: ['#1754EF', '#0F71BD', '#20BDDE']
+
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById('bargraph'));
+
+        chart.draw(chartdata, options);
+    });
 }
-
-
-
-
-
 
 // --------------こっから円グラフ--------------------------
 //コンテンつ
-function create_chart_2() {
-    google.load("visualization", "1", { packages: ["corechart"] });
-    google.setOnLoadCallback(drawChart);
+google.load("visualization", "1", { packages: ["corechart"] });
+google.setOnLoadCallback(drawChart_2);
 
-    function drawChart() {
-        var df = $.Deferred();
+function drawChart_2() {
+    var df = $.Deferred();
 
-        var sum_1 = 0;
-        var sum_2 = 0;
-        var sum_3 = 0;
+    var sum_1 = 0;
+    var sum_2 = 0;
+    var sum_3 = 0;
 
-        $(function() {
-            $.ajax({
-                url: 'study_contents.json',
-                dataType: 'json',
-            }).done(function(data) {
-                console.log("success");
-                $(data).each(function() {
-                    sum_1 = sum_1 + this.c_1;
-                    sum_2 = sum_2 + this.c_2;
-                    sum_3 = sum_3 + this.c_3;
-                });
-
-                df.resolve();
-            }).fail(function() {
-                console.log("error");
+    $(function() {
+        $.ajax({
+            url: 'study_contents.json',
+            dataType: 'json',
+        }).done(function(data) {
+            console.log("success");
+            $(data).each(function() {
+                sum_1 = sum_1 + this.c_1;
+                sum_2 = sum_2 + this.c_2;
+                sum_3 = sum_3 + this.c_3;
             });
+
+            df.resolve();
+        }).fail(function() {
+            console.log("error");
         });
+    });
 
-        df.done(function() {
-            var chartdata_2 = google.visualization.arrayToDataTable([
-                ['day', 'contents'],
-                ['N予備校', sum_1],
-                ['ドットインストール', sum_2],
-                ['posse課題', sum_3]
-            ]);
-            console.log(chartdata_2);
-            var options = {
-                // title: 'Age vs. Weight comparison',
-                //hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
-                //vAxis: { title: 'Weight', minValue: 0, maxValue: 15 },
-                legend: 'none',
-                //legend:{position:'bottom'},
-
-                'chartArea': { top: 0, 'width': '100%', 'height': '100%' },
-                pieHole: 0.5,
-                colors: ['#1754EF', '#0F71BD', '#20BDDE'],
-
-            };
-            var chart_2 = new google.visualization.PieChart(document.getElementById('pieChart_contents'));
-
-            chart_2.draw(chartdata_2, options);
-        });
-    }
+    df.done(function() {
+        var chartdata_2 = google.visualization.arrayToDataTable([
+            ['day', 'contents'],
+            ['N予備校', sum_1],
+            ['ドットインストール', sum_2],
+            ['posse課題', sum_3]
+        ]);
+        console.log(chartdata_2);
+        var options = {
+            legend: 'none',
+            'chartArea': { top: 0, 'width': '100%', 'height': '100%' },
+            pieHole: 0.5,
+            colors: ['#1754EF', '#0F71BD', '#20BDDE'],
+        };
+        var chart_2 = new google.visualization.PieChart(document.getElementById('pieChart_contents'));
+        chart_2.draw(chartdata_2, options);
+    });
 }
+
 
 
 
 //言語
-function create_chart_3() {
-    google.load("visualization", "1", { packages: ["corechart"] });
-    google.setOnLoadCallback(drawChart);
+google.load("visualization", "1", { packages: ["corechart"] });
+google.setOnLoadCallback(drawChart_3);
 
-    function drawChart() {
-        var df = $.Deferred();
+function drawChart_3() {
+    var df = $.Deferred();
 
-        var sum_1 = 0;
-        var sum_2 = 0;
-        var sum_3 = 0;
-        var sum_4 = 0;
-        var sum_5 = 0;
-        var sum_6 = 0;
-        var sum_7 = 0;
-        var sum_8 = 0;
+    var sum_1 = 0;
+    var sum_2 = 0;
+    var sum_3 = 0;
+    var sum_4 = 0;
+    var sum_5 = 0;
+    var sum_6 = 0;
+    var sum_7 = 0;
+    var sum_8 = 0;
 
-        $(function() {
-            $.ajax({
-                url: 'study_language.json',
-                dataType: 'json',
-            }).done(function(data) {
-                console.log("success");
-                $(data).each(function() {
-                    sum_1 = sum_1 + this.l_1;
-                    sum_2 = sum_2 + this.l_2;
-                    sum_3 = sum_3 + this.l_3;
-                    sum_4 = sum_4 + this.l_4;
-                    sum_5 = sum_5 + this.l_5;
-                    sum_6 = sum_6 + this.l_6;
-                    sum_7 = sum_7 + this.l_7;
-                    sum_8 = sum_8 + this.l_8;
-                });
-
-                df.resolve();
-            }).fail(function() {
-                console.log("error");
+    $(function() {
+        $.ajax({
+            url: 'study_language.json',
+            dataType: 'json',
+        }).done(function(data) {
+            console.log("success");
+            $(data).each(function() {
+                sum_1 = sum_1 + this.l_1;
+                sum_2 = sum_2 + this.l_2;
+                sum_3 = sum_3 + this.l_3;
+                sum_4 = sum_4 + this.l_4;
+                sum_5 = sum_5 + this.l_5;
+                sum_6 = sum_6 + this.l_6;
+                sum_7 = sum_7 + this.l_7;
+                sum_8 = sum_8 + this.l_8;
             });
+
+            df.resolve();
+        }).fail(function() {
+            console.log("error");
         });
+    });
 
-        df.done(function() {
-            var chartdata_2 = google.visualization.arrayToDataTable([
-                ['day', 'contents'],
-                ['HTML', sum_1],
-                ['CSS', sum_2],
-                ['JavaScript', sum_3],
-                ['PHP', sum_4],
-                ['Laraver', sum_5],
-                ['SQL', sum_6],
-                ['SHELL', sum_7],
-                ['情報基礎知識', sum_8]
-            ]);
-            var options = {
-                // title: 'Age vs. Weight comparison',
-                //hAxis: { title: 'Age', minValue: 0, maxValue: 15 },
-                //vAxis: { title: 'Weight', minValue: 0, maxValue: 15 },
-                legend: 'none',
-                'chartArea': { top: 0, 'width': '100%', 'height': '100%' },
-                pieHole: 0.5,
-                colors: ['#1754EF', '#0F71BD', '#20BDDE', '#3CCEFE', '#B29EF3', '#6D46EC', '#4A17EF', '#3105C0'],
+    df.done(function() {
+        var chartdata_2 = google.visualization.arrayToDataTable([
+            ['day', 'contents'],
+            ['HTML', sum_1],
+            ['CSS', sum_2],
+            ['JavaScript', sum_3],
+            ['PHP', sum_4],
+            ['Laraver', sum_5],
+            ['SQL', sum_6],
+            ['SHELL', sum_7],
+            ['情報基礎知識', sum_8]
+        ]);
+        var options = {
+            legend: 'none',
+            'chartArea': { top: 0, 'width': '100%', 'height': '100%' },
+            pieHole: 0.5,
+            colors: ['#1754EF', '#0F71BD', '#20BDDE', '#3CCEFE', '#B29EF3', '#6D46EC', '#4A17EF', '#3105C0'],
 
-            };
-            var chart_2 = new google.visualization.PieChart(document.getElementById('pieChart_language'));
+        };
+        var chart_2 = new google.visualization.PieChart(document.getElementById('pieChart_language'));
 
-            chart_2.draw(chartdata_2, options);
-        });
-    }
+        chart_2.draw(chartdata_2, options);
+    });
 }
-create_chart_1();
-create_chart_2();
-create_chart_3();
-
 
 window.onresize = function() {
-    create_chart_1();
-    create_chart_2();
-    create_chart_3();
+    google.setOnLoadCallback(drawChart);
+    google.setOnLoadCallback(drawChart_2);
+    google.setOnLoadCallback(drawChart_3);
 }
