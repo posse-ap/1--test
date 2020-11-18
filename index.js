af92ab0f2a03826784aabd91c716de58faf1814d
湯山智晴 <tomoharuyuyama@gmail.com>
2020-11-18 19:47:44 +0900

    代理プルリク

---
+++ index.js
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+$(function () {
+    $('#openModal').click(function(){
+        $('#modalArea').fadeIn();
+    });
+    $('#closeModal , #modalBg').click(function(){
+      $('#modalArea').fadeOut();
+    });
+=======
+$(function () {
+    $('#openModal').click(function(){
+        $('#modalArea').fadeIn();
+    });
+    $('#closeModal , #modalBg').click(function(){
+      $('#modalArea').fadeOut();
+    });
+>>>>>>> 7a9482cb2413ddd4bbf3e85fdc40c78930d696a2
+  });
