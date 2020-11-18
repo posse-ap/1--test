8277ce167155f86243249526e761e1ed18e9afd5
tomoharuyuyama <62699348+tomoharuyuyama@users.noreply.github.com>
2020-11-19 08:14:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(function () {
     $('#openModal').click(function(){
         $('#modalArea').fadeIn();
@@ -6,12 +6,4 @@ $(function () {
     $('#closeModal , #modalBg').click(function(){
       $('#modalArea').fadeOut();
     });
-=======
-$(function () {
-    $('#openModal').click(function(){
-        $('#modalArea').fadeIn();
-    });
-    $('#closeModal , #modalBg').click(function(){
-      $('#modalArea').fadeOut();
-    });
->>>>>>> 7a9482cb2413ddd4bbf3e85fdc40c78930d696a2
+
