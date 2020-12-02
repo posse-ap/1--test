5556078021a789486809db76150fb16d89975a78
湯山智晴 <tomoharuyuyama@gmail.com>
2020-12-02 17:42:47 +0900

    上げ直し

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(function () {
     $('#openModal').click(function(){
         $('#modalArea').fadeIn();
@@ -5,4 +6,12 @@ $(function () {
     $('#closeModal , #modalBg').click(function(){
       $('#modalArea').fadeOut();
     });
-  });
+=======
+$(function () {
+    $('#openModal').click(function(){
+        $('#modalArea').fadeIn();
+    });
+    $('#closeModal , #modalBg').click(function(){
+      $('#modalArea').fadeOut();
+    });
+>>>>>>> 7a9482cb2413ddd4bbf3e85fdc40c78930d696a2
